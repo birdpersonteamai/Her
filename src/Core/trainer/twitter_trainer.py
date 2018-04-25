566e96749658421c5d1b0b3501fc37d66eb85466
Daryl Fung <darylfung96@gmail.com>
2018-04-24 22:49:21 -0500

    finalized config file

--- src/Core/trainer/twitter_trainer.py
+++ src/Core/trainer/twitter_trainer.py
@@ -2,7 +2,7 @@ import os
 
 def train_twitter_dataset():
     config_file = os.path.join(os.path.dirname(__file__), '../', '../', '../', 'config', 'transformer.yml')
-    os.system('onmt-main train_and_eval --model_type Transformer --config {}'.format(config_file))
+    os.system('onmt-main train --model_type Transformer --config {}'.format(config_file))
 
 if __name__ == '__main__':
     train_twitter_dataset()
